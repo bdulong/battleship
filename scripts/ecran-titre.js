2f78a663ee9808899b4786549f176c9b9080e0dc
bdulong <baptistedulong@gmail.com>
2024-01-06 00:35:47 +0100

    ok ?

---
+++ scripts/ecran-titre.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var boutonJouer = document.querySelector('.bouton__jouer');
+    var ecranTitre = document.querySelector('.ecran-titre');
+    var placementBateau = document.querySelector('.placement-bateau');
+
+    boutonJouer.addEventListener('click', function() {
+        ecranTitre.style.display = 'none';
+        placementBateau.style.display = 'flex';
+    });
+});
